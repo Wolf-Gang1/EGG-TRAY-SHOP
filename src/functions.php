53490ff6ff679e0502430c27c5b04688a02f6ac6
Wolf-Gang1 <105771700+Wolf-Gang1@users.noreply.github.com>
2025-09-16 16:13:42 +0800

    functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,15 @@
-<?php /* Helper functions */ ?>
+<?php
+require_once __DIR__ . '/db.php';
+function format_money($amount) {
+return number_format((float)$amount, 2);
+}
+function append_transaction_csv($orderRef, $total, $status, $created_at) {
+$file = __DIR__ . '/../data/transactions.csv';
+$exists = file_exists($file);
+$fh = fopen($file, 'a');
+if (!$exists) {
+fputcsv($fh, ['order_ref','total','status','created_at']);
+}
+fputcsv($fh, [$orderRef, $total, $status, $created_at]);
+fclose($fh);
+}
