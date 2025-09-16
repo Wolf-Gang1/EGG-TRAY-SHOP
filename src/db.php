4a8da7e7aaeb8f8b7d245150f8438bf44e17a5a1
Wolf-Gang1 <105771700+Wolf-Gang1@users.noreply.github.com>
2025-09-16 16:16:05 +0800

    Update db.php

--- src/db.php
+++ src/db.php
@@ -6,7 +6,6 @@ $DB_PASS = '';
 $DB_NAME = 'egg_tray_shop';
 try {
 $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",
-2
 $DB_USER, $DB_PASS, [
 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
